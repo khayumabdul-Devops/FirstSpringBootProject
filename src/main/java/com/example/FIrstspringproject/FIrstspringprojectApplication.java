849197fea44e04d28c088c6e3bdf5040d6ad28d1
Abdul <abdulkhayumshaikh@gmail.com>
2026-08-23 11:35:57 +0530

    Intial COmmit

---
+++ src/main/java/com/example/FIrstspringproject/FIrstspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FIrstspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIrstspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FIrstspringprojectApplication.class, args);
+	}
+
+}
