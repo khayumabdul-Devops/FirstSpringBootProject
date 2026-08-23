7986a056fd2e22df55f9c2716be2caf80bdaf7e0
Abdul <abdulkhayumshaikh@gmail.com>
2026-08-23 16:51:10 +0530

    commented sop code

--- src/main/java/com/example/FIrstspringproject/FIrstspringprojectApplication.java
+++ src/main/java/com/example/FIrstspringproject/FIrstspringprojectApplication.java
@@ -8,7 +8,7 @@ public class FIrstspringprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FIrstspringprojectApplication.class, args);
-		System.out.println("Welcome to SpringBoot");
+		//System.out.println("Welcome to SpringBoot");
 	}
 
 }
